87a22042e55622edf8972c537a9be7ab980fddd8
Michael Schappacher <m.a.schappacher@gmail.com>
2022-09-29 14:15:30 -0400

    Add Files for automating publishing package

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+import sdist_upip
+
+with open("README.MD", "r") as long_description_file_reader:
+    long_description = long_description_file_reader.read()
+
+setup(
+    name='micropython-picowebrouter',
+    version="v0.0.1",
+    description="Pi Pico webserver",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/MikeSchapp/PicoWebRouter',
+    author='MikeSchapp',
+    keywords='micropython, webserver',
+    project_urls={
+        'Source': 'https://github.com/MikeSchapp/PicoWebRouter',
+    },
+    license='BSD 2',
+    packages=['picowebrouter', 'picowebrouter/objects'],
+    cmdclass={'sdist': sdist_upip.sdist}
+)
+
