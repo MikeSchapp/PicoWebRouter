0312220b2017067de9f98ac64ebb5aca377ee1d1
Michael Schappacher <m.a.schappacher@gmail.com>
2022-10-03 09:26:07 -0400

    Change how request object is instantiated from raw request

--- picowebrouter/objects/request.py
+++ picowebrouter/objects/request.py
@@ -1,34 +1,53 @@
 from ..utils import unquote
 class Request:
-    def __init__(self, request):
-        self.raw = request
-        self.method = None
-        self.path = None
-        self.query_strings = None
-        self.protocol = None
-        self.headers = None
-        self.body = None
-        self.parse_request()
+    def __init__(self,
+        raw=None,
+        method=None,
+        path=None,
+        query_strings=None,
+        protocol=None,
+        headers=None,
+        body=None
+    ):
+        self.raw = raw
+        self.method = method
+        self.path = path
+        self.query_strings = query_strings
+        self.protocol = protocol
+        self.headers = headers
+        self.body = body
 
-    def parse_request(self):
+    
+    @classmethod
+    def parse_request(cls, request):
+        body = None
         try:
-            request, self.body = self.raw.split("\r\n\r\n")
+            request, body = request
         except ValueError:
-            request = self.raw
+            request = request
         split_request = request.split("\r\n")
         request_line = split_request.pop(0)
-        self.method, self.path, self.protocol = request_line.split(" ")
-        if "?" in self.path:
-            query_strings = {}
-            self.path, raw_query = self.path.split("?")
+        method, path, protocol = request_line.split(" ")
+        query_strings = {}
+        if "?" in path:
+            path, raw_query = path.split("?")
             raw_query = raw_query.split("&")
             for query in raw_query:
                 key, value = query.split("=")
                 query_strings[key] = unquote(value).decode("utf-8")
-            self.query_strings = query_strings
+            query_strings = query_strings
 
         headers = {}
         for header in split_request:
             key, value = header.split(": ")
             headers[key] = value
-        self.headers = headers
+        headers = headers
+        return cls(
+            request,
+            method,
+            path,
+            query_strings,
+            protocol,
+            headers,
+            body
+        )
